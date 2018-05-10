063285dfff105690cb3992cb95f183cfdc706441
Cristian Sandu <cristian.sandu@rinftech.com>
2018-05-10 17:33:38 +0300

    es6

--- src/basics.js
+++ src/basics.js
@@ -3,14 +3,14 @@
 // ------------------------------------------------
 
 // Create an empty scene
-var scene = new THREE.Scene();
+let scene = new THREE.Scene();
 
 // Create a basic perspective camera
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 4;
 
 // Create a renderer with Antialiasing
-var renderer = new THREE.WebGLRenderer({
+let renderer = new THREE.WebGLRenderer({
 	antialias: true
 });
 
@@ -28,22 +28,26 @@ document.body.appendChild(renderer.domElement);
 // ------------------------------------------------
 
 // Create a Cube Mesh with basic material
-var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshBasicMaterial({
+let geometry = new THREE.BoxGeometry(1, 1, 1);
+let material = new THREE.MeshBasicMaterial({
 	color: 'green'
 });
-var cube = new THREE.Mesh(geometry, material);
+let cube = new THREE.Mesh(geometry, material);
 
 // Add cube to Scene
 scene.add(cube);
 
 // Render Loop
-var render = function () {
+let render = function () {
 	requestAnimationFrame(render);
 
 	// cube.rotation.x += 0.01;
 	// cube.rotation.y += 0.01;
 	// cube.rotation.z += 0.01;
+
+    let dest = new THREE.Vector3(1, 1, 1);
+    let rotMatrix = new THREE.Matrix4().lookAt(dest, new THREE.Vector3(0, 1, 0));
+
 
 	cube.rotateX(0.01);
 
