291797f56ece8e9441590c52a538ce3c7681914d
Cristian Sandu <cristian.sandu@rinftech.com>
2018-08-21 11:45:40 +0300

    also translate head

--- src/basics.js
+++ src/basics.js
@@ -137,8 +137,6 @@ function run() {
 			} else {
 				clip.stop();
 				mixerRunning = false;
-
-				updateAngles(false);
 			}
 		} else if (event.key === '-' || event.key === '+') {
 			let increment = event.key  === '+' ? 0.2 : -0.2;
@@ -153,8 +151,19 @@ function run() {
 			console.log('Clip weight: ', clipWeight);
 		} else if (event.key === 'o') {
 			animated = !animated;
+		} else if (event.keyCode === 37) { 
+			// LEFT
+			head.position.x -= 1;
+		} else if (event.keyCode === 38) {
+			// UP
+			head.position.y += 1;
+		} else if (event.keyCode === 39) {
+			// RIGHT
+			head.position.x += 1;
+		} else if (event.keyCode === 40) {
+			// DOWN
+			head.position.y -= 1;
 		}
-
 		updateAngles();
 	}
 
@@ -169,6 +178,7 @@ function run() {
 	let interpolationFactor = 0;
 
 	function updateAngles(reset = true) {
+		head.updateMatrixWorld(true);
 		// leftEye.updateMatrixWorld(true);
 		// rightEye.updateMatrixWorld(true);
 
